4bf919bba7d99cd6db722bf4b1015ccb2ce2badf
Purity Wambua <puritywamb@gmail.com>
2024-08-07 14:37:51 +0300

    information flow

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,5 +1,48 @@
+//The old shoppingList.js
+
+// import React, { useState } from "react";
+// import Item from "./Item";
+// import Filter from "./Filter";
+
+
+// function ShoppingList({ items }) {
+//   const [selectedCategory, setSelectedCategory] = useState("All");
+
+//   function handleCategoryChange(event) {
+//     setSelectedCategory(event.target.value);
+//   }
+
+//   const itemsToDisplay = items.filter((item) => {
+//     if (selectedCategory === "All") return true;
+
+//     return item.category === selectedCategory;
+//   });
+
+//   return (
+//     <div className="ShoppingList">
+//       <div className="Filter">
+//         <select name="filter" onChange={handleCategoryChange}>
+//           <option value="All">Filter by category</option>
+//           <option value="Produce">Produce</option>
+//           <option value="Dairy">Dairy</option>
+//           <option value="Dessert">Dessert</option>
+//         </select>
+//       </div>
+//       <ul className="Items">
+//         {itemsToDisplay.map((item) => (
+//           <Item key={item.id} name={item.name} category={item.category} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default ShoppingList;
+
+//updated ShoppingList 
 import React, { useState } from "react";
 import Item from "./Item";
+import Filter from "./Filter";  // Import the new Filter component
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
@@ -16,14 +59,7 @@ function ShoppingList({ items }) {
 
   return (
     <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
+      <Filter onCategoryChange={handleCategoryChange} />  {/* Use Filter */}
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
